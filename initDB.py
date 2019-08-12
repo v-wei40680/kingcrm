9fea53317366eecf270b131f4c36994824def2f1
v-wei40680 <wei40680@pm.me>
2019-08-12 16:38:41 +0800

    修复合同审核

--- initDB.py
+++ initDB.py
@@ -36,15 +36,34 @@ except:
     up1 = UserProfile.objects.first()
 up1.role.add(r1)
 
-b1 = Branch(name='大学城')
-b1.save()
+try:
+    b1 = Branch(name='大学城')
+    b1.save()
+except:
+    b1 = Branch.objects.first()
 
-c1 = Course(name='python基础', price=999, period=1, outline='nothing')
-c1.save()
+try:
+    c1 = Course(name='python基础', price=999, period=1, outline='nothing')
+    c1.save()
+except:
+    c1 = Course.objects.first()
 
-ct1 = Contract(name='python基础合同', content='合同内容')
-ct1.save()
+try:
+    ct1 = ContractTemplate(name='python基础合同', content='合同内容')
+    ct1.save()
+except:
+    ct1 = ContractTemplate.objects.first()
 
-cl1 = ClassList(branch=b1, course=c1, semester=1, teachers=u1, start_date=datetime.date.today)
-cl1.save()
-cl1.teachers.add(u1)
+try:
+    cl1 = ClassList(branch=b1, course=c1, semester=1, start_date=datetime.date.today())
+    cl1.save()
+except:
+    cl1 = ClassList.objects.first()
+cl1.teachers.add(up1)
+
+s1 = Student(customer=ci1)
+s1.save()
+s1.class_grades.add(cl1)
+
+stuEnroll1 = StudentEnrollment(customer=ci1, class_grade=cl1,  consultant=up1)
+stuEnroll1.save()
